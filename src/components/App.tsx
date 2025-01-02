22311aeae92cc1df6ce524cdc9e08b27f279deb9
KirillMatviychuck <Kirill M>
2025-01-02 19:01:22 +0200

    fixed suspense bug

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,16 @@
-import { Link, Route, Routes } from "react-router-dom"
+import { Link, Route, Routes, useLocation } from "react-router-dom"
 import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "../pages/MainPage/MainPage.async"
 import './index.scss'
 import { Suspense } from "react"
 
 export const App = () => {
+    const location = useLocation();
     return (
         <div className="app">
             <Link to='/'>MainPage</Link>
             <Link to='/about'>AboutPage</Link>
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense fallback={<div>Loading...</div>} key={location.pathname}>
                 <Routes>
                     <Route path='/' element={<MainPageAsync />} />
                     <Route path='/about' element={<AboutPageAsync />} />
