9b281bf18345d30b089275c84e48603627086a86
KirillMatviychuck <k.matviychuck@gmail.com>
2025-01-03 12:31:41 +0200

    decomposed the routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,20 @@
-import { Suspense } from "react";
-import { Link, Route, Routes, useLocation } from "react-router-dom";
-import { classNames } from "shared/lib/classNames/classNames";
 import 'app/styles/_index.scss';
+import { Link, useLocation } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
-import { routeConfig } from "./providers/router/ui/AppRouter";
+import AppRoute from './providers/router/ui/AppRoute';
 
 
 
 
 export const App = () => {
-    const location = useLocation();
     const { theme, toggleTheme } = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Link to='/'>MainPage</Link>
             <Link to='/about'>AboutPage</Link>
-            <Suspense fallback={<div>Loading...</div>} key={location.pathname}>
-                <Routes>
-                    {Object.values(routeConfig).map(({ element, path }) => (
-                        <Route
-                            key={path}
-                            path={path}
-                            element={element}
-                        />
-                    ))}
-                </Routes>
-            </Suspense>
+            <AppRoute />
             <div>
                 <button onClick={toggleTheme}>TOGGLE THEME</button>
             </div>
