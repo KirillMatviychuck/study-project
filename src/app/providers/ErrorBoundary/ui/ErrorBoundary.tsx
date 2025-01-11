8e5e999076aaf9b92ddbd8cfaa57c5128d8b0d2a
KirillMatviychuck <k.matviychuck@gmail.com>
2025-01-11 21:43:33 +0200

    add ErrorBoundary and ErrorPage

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ErrorInfo, ReactNode } from "react";
+import { ErrorPage } from "widgets/ErrorPage";
+
+interface ErrorBoundaryProps {
+    children: ReactNode
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, info: ErrorInfo) {
+        console.log(error, info.componentStack);
+    }
+
+    render() {
+        const { hasError } = this.state;
+        const { children } = this.props
+
+        if (hasError) {
+
+            return <ErrorPage />
+        }
+
+        return children;
+    }
+}
+
+export default ErrorBoundary;
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,14 @@ import { App } from './app/App'
 import { ThemeProvider } from 'app/providers/ThemeProvider'
 
 import 'shared/config/i18n/i18n'
+import { ErrorBoundary } from 'app/providers/ErrorBoundary'
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>
 )
